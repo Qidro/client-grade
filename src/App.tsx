d337cb3f9bdb1b1f31bb5e40ffb2ae100d452b8a
Osincev Pavel <pavel.osincev04@mail.ru>
2024-09-23 15:45:07 +0500

    attempt a setting route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import './App.css';
 import { Reg } from'./components/Registration/Registration';
 import { feact } from './services/nodes';
+import { Route, Router, Routes } from 'react-router-dom';
 import { Aut } from './components/Authorization/Authorization';
 
 function App() {
@@ -14,7 +15,12 @@ function App() {
   },[])
 
   return (
-    <Aut/>
+    <Routes>
+      <Route path='/reg' element={<Reg />}>
+      </Route>
+      <Route path='/' element={<Aut />}>
+      </Route>
+  </Routes>
   );
 }
 
